9d9dfc94005454aaa70e2a100941ed2a35e1017c
Ria <chocolodoro@gmail.com>
2016-10-26 11:04:19 -0400

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -2,14 +2,14 @@
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-   /* $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
-    */
+    
       $("#nav ul li a[href^='#']").on('click', function(e) {
         e.preventDefault();
         
